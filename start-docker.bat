7282b2e3b9fdfd86585260ce5a2814d5f57497e4
GuusjeB <guusjeboomgaard@hotmail.com>
2019-01-22 11:23:11 +0100

    changed path in start-docker file

--- start-docker.bat
+++ start-docker.bat
@@ -1,2 +1,2 @@
-SET project_folder=/c/Users/Guusje/Documents/Education/Artificial_Intelligence/GitLM/learning_machines_robobo-master
+SET project_folder=/c/Users/Guusje/Documents/Education/Artificial_Intelligence/GitLM/learning_machines_robobo
 docker run --rm -it -v %project_folder%:/root/projects cigroup/learning-machines bash
