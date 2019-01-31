c5f86e987ff3ac9eb22ef22c8ec2592b20ef78c6
GuusjeB <guusjeboomgaard@hotmail.com>
2019-01-31 13:01:51 +0100

    nieuwe draairichtingen

--- src/draaien.py
+++ src/draaien.py
@@ -5,25 +5,35 @@ import time
 import numpy as np
 
 if __name__ == "__main__":
-    rob = robobo.HardwareRobobo(camera=True).connect(address="192.168.1.21")
+    rob = robobo.HardwareRobobo(camera=False).connect(address="172.20.10.11")
 
-    # rechtdoor
-    # rob.move(20, 20, 1000)
-    # time.sleep(0.1)
+#rechtdoor
+#rob.move(20, 20, 1000)
+#time.sleep(0.1)
 
-    # 45 graden naar rechts
-    # rob.move(20, -5, 1000)
-    rob.move(10, -5, 1000)
+# 30 graden naar rechts
+rob.move(15, -5, 1000)
 
-    # 90 graden naar rechts
-    # rob.move(50, -5, 1000)
+# 30 graden naar links
+rob.move(-5, 15, 1000)
 
-    # 45 graden naar links
-    # rob.move(-5, 20, 1000)
-    # rob.move(-5, 10, 1000)
+# 120 graden naar links
+rob.move(-5, 90, 1000)
 
-    # 90 graden naar links
-    # rob.move(-5, 50, 1000)
+# 90 graden naar rechts
+rob.move(55, -5, 1000)
+
+# 20 graden naar rechts 
+# rob.move(10, -5, 1000)
+
+# 45 graden naar rechts
+# rob.move(20, -5, 1000)
+
+# 45 graden naar links
+#rob.move(-5, 20, 1000)
+
+# 90 graden naar links
+#rob.move(-5, 50, 1000)
 
         # if 0 <= o < 0.2:
         #     rob.move(20, 20, 1000)
